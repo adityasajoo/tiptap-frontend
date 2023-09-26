28cae1466ecb97a81a11d0824697bdee2eebd7af
Adi <adityasajoo@gmail.com>
2023-09-26 10:08:41 +0530

    Feat: Add yjs

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -1,9 +1,21 @@
 // src/Tiptap.jsx
+import Collaboration from '@tiptap/extension-collaboration';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import { WebsocketProvider } from 'y-websocket';
+import * as Y from 'yjs';
+
+const doc = new Y.Doc();
+//Add the websocket URL here
+new WebsocketProvider('ws://localhost:1234', 'my-roomname', doc);
 
 // define your extension array
-const extensions = [StarterKit];
+const extensions = [
+  StarterKit,
+  Collaboration.configure({
+    document: doc,
+  }),
+];
 
 const content = '<p>Hello World!</p>';
 
